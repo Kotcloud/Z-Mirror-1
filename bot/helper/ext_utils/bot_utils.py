be8d8eab112741937b353abf34418728cc62d8a3
Kotcloud <115068885+Kotcloud@users.noreply.github.com>
2022-10-07 10:39:24 +0530

    check

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -163,7 +163,7 @@ T: {disk_t} | F: {disk_f}
 Working For: {currentTime}
 T-DL: {recv} | T-UL: {sent}
 
-Made with ❤️ by Dawn
+Made with ❤️ by @KOT_BOTS | @KOT_FREE_DE_LA_HOYA_OFF
 """
 
 #---Thanks for deleting my name ❤️ Appreciate it---#
